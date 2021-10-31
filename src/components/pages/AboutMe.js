09a3546b2ba3a8bc8fd01fdcb83f5511edeb09c3
Anna An <arzumanyan.an@gmail.com>
2021-10-31 11:31:04 +0400

    some changes

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,7 +1,15 @@
 import React from "react";
+const title = "About me section";
+const aboutText =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore beatae,\n nam quisquam voluptatum sint ipsam? Totam et est provident aliquam nihil \n aperiam accusantium tempora veritatis magnam, culpa voluptatibus ratione \n ducimus!";
 
 function AboutMe() {
-  return <div className="aboutMe">About me section</div>;
+  return (
+    <div className="aboutMe">
+      {title} <br />
+      {aboutText}
+    </div>
+  );
 }
 
 export default AboutMe;
